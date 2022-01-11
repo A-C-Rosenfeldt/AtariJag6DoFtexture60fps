ebcc7c5a38ffefd50aef366ff7371f2de6a97cc7
Arne Rosenfeldt <Arne.Rosenfeldt@gmail.com>
2022-01-11 11:10:26 +0100

    edge clipping

--- mesh.asm
+++ mesh.asm
@@ -23,8 +23,53 @@
 
 ;https://en.wikipedia.org/wiki/Line_(geometry)
 ; Would be cool to have pure Multiplication code to check if edge crosses screen border ( early out if vertices are not on different sides)
-; shear transformation using two frustum edges and the edge in question
-; CMP vertex position
+; The edge of the frustum [0,1,2] cross the direction of the edge [4,5,6] gives us a plane with a normal [8,9,A]
+
+{
+	{
+		NEG 4
+		IMULT 0,5
+		IMAC  4,1
+		RESMAC A
+		NEG 5
+		IMULT 1,6
+		IMAC  5,2
+		RESMAC 8
+		NEG 4
+		NEG 6
+		IMULT 2,4
+		IMAC  6,0
+		RESMAC 9 
+		;NEG 9
+
+		; The inner product between normal and vertex [CDE] tells us if the vertex is inside and thus this edge crosses the edge of the screen ; Alternatively the roles of vertex and edge direction can be swapped ( to reduce number of calculations depending of mesh "topology" )
+		IMULT 8,C
+		IMAC  9,E
+		IMAC  A,E
+		RESMAC F  ; flags unaffected
+
+		;store
+		OR F,F
+		XOR 10,10
+		BSET 31,10
+		AND 10,F   ; sign
+		OR F,11    ; store
+		RORQ 30,11  ; one wrap around to lower bits
+
+		; next frustum edge
+		NEG 0
+		; RORQ in delay slot
+	}
+	JR N,
+	NEG 1
+	NOP
+}
+JR N,
+
+;11 has 16 different values. 4 of them mean that the edge is off screen.
+
+
+; Repeat for all corners
 
 MOVE B,3 ; backup deltaY   ; todo swap register assignment
 JR Z , never render this edge
@@ -59,6 +104,14 @@ REASMAC 5  ; for Bresenham
 ; A triangle can be visible if only one edge is visble => screen corners become other vertices.
 ; A triangle may be visible without any edge or vertex on screen .. all corner become vertices.
 ; Use raytracing to determine [W,U,V] at the corners. Early out if W is behind camera. Multiply with W to get U and V.
+; For triangles with textureCount != 1 or with Gouraud  .. we better argue using interpolation (s,t)
+; interpolation of the vertices need to result in the same point as frustum corner scaled ( we choose w as variable)
+; Matrix inversion gives us s,t,w
+;https://en.wikipedia.org/wiki/Invertible_matrix#Inversion_of_3_%C3%97_3_matrices
+; Inversion consists of 3 cross products ( I use cross products elsewhere )
+; Inner product of last column with cross product ( backup copy) of first two give denominator
+;DIV
+; Matrix multiplication (MMULT) gives us Gouraud, U, V
 
 
 ;Backface culling
@@ -93,6 +146,10 @@ SUBQ 1,2
 JR C, return
 
 
+; small tris write phrases
+; large tris write 4 stacked phrases
+;; RWM .. no come on. Don't access phrases outside of triangle in external RAM!
+;;; If code blows up, align tiles to the center vertex
 
 ;;;;;;;;;;; rasterize large triangles over 8x8 tiles  ( I start with affine .. perspective is later)
 ;; use bilinear if all corners are inside
