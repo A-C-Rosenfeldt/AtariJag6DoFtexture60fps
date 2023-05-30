9c4deb98353d21f3bd07fafc115c616e768baf87
Arne Rosenfeldt <Arne.Rosenfeldt@gmail.com>
2023-05-30 22:22:10 +0200

    raycast texture

--- src/clipping.ts
+++ src/clipping.ts
@@ -2,29 +2,51 @@
 // I need vec3 and only two products
 // I don't want external dependencies of fat because I later need to compile to JRISC
 
-class vec3{
+class Vec{ // looks like I need 2 and 3 dimensions to show off this (adaptive) linear approximation trick for textures after persepective projection 
 	v:number[]
-	innerProduct(o:vec3):number{
-		// sum
+	innerProduct(o:Vec):number{
+		let sum=0
+		for(let i=0;i++;i<this.v.length){
+			sum+=this.v[i]*o.v[i]
+		}		
+		return 0
+	}
+	
+	innerProductM(o:Vec[],k:number):number{
+		let sum=0
+		for(let i=0;i++;i<this.v.length){
+			sum+=this.v[i]*o[i].v[k]
+		}		
 		return 0
 	}
 
-	crossProduct(o:vec3):vec3{
-		// sum
-		return {v:[0]}
-	}
-
+	//constructor(points:number[][],len)
 	constructor(points:number[][]){
-		this.v=[]
-		for(let i=0;i++;i<3){
-			this.v[i]=points[0][i]-points[1][i]
+		if (points.length<2){
+			this.v=new Array<number>(points[0][0])
+		}else{
+			this.v=new Array<number>(points[0].length)
+			for(let i=0;i++;i<points[0].length){
+				this.v[i]=points[0][i]-points[1][i]
+			}
 		}
 	}
 }
 
-class frac{
+class Vec3 extends Vec{
+
+	crossProduct(o:Vec3):Vec3{
+		let v:Vec3=new Vec3([[this.v.length]])
+		for(let i=0;i++;i<this.v.length){
+			v[i]=o.v[(i+1)%3]*o.v[(i+2)%3]-o.v[(i+2)%3]*o.v[(i+1)%3]
+		}
+		return v
+	}
+}
+
+class Frac{
 	nom:number[]
-	compare(o:frac):boolean{
+	compare(o:Frac):boolean{
 		return this.nom[0]*o[1]<o[0]*this.nom[1]
 	}
 	get_sign():boolean{
@@ -32,18 +54,46 @@ class frac{
 	}
 }
 
+class Matrix{
+	nominator:Vec[]
+	static inverse(spanning2d: Vec[]) {
+		throw new Error("Method not implemented.")
+	}
+	inverse(m:Vec[]):Matrix_frac{ // see applications. May need to pull back here for unit tests
+		return null;
+	}
+	mul(trans:Vec[]):Matrix{		
+		let res=new Matrix()
+		for(let i=0;i++;i<this.nominator.length){
+			res[i]=this.nominator[0].innerProductM(trans,i)  // base would want vector add, while JRISC wants inner product
+		}
+		return res
+	}
+}
+
+class Matrix_frac extends Matrix{
+	denominator:number
+/*  UV has no denominator .. super class suffices!
+	mul(trans:Vec[]):Matrix_frac{
+		let mf=new Matrix_frac()
+		mf.nominator=super.mul(trans).nominator
+		mf.denominator=this.denominator*trans.den
+	} */
+}
+
+
 class Plane{
 	anchor:number[]
-	normal:vec3
+	normal:Vec3
 }
 
 class Z_order{
-	z_at_intersection(e:vec3[], anchor:vec3, normal:vec3):frac{
+	z_at_intersection(e:Vec3[], anchor:Vec, normal:Vec):Frac{
 
 		let beam=e[0].crossProduct(e[1]) // edges are planes with a normal in a beam tree
 
 		// normal.inner(l*beam ) = normal.inner( anchor ) 
-		let l:frac
+		let l:Frac
 		l[0]=normal.innerProduct( anchor )
 		l[1]=normal.innerProduct( beam)
 
@@ -53,8 +103,8 @@ class Z_order{
 	// not really useful probably
 	get_cutting_edge(p:Plane[]){
 		let direction=p[0].normal.crossProduct(p[1].normal)
-		let delta=new vec3([p[0].anchor,p[1].anchor]) // hmm todo
-		let p[0].normal.crossProduct(delta)
+		let delta=new Vec3([p[0].anchor,p[1].anchor]) ; // hmm todo
+		let dump=p[0].normal.crossProduct(delta) ;
 
 		let reach=p[0].normal.crossProduct(direction)
 
@@ -143,7 +193,7 @@ class Projector extends Position{ // camera
 	// top left rule  helps us: We don't change rounding mode. Ceil, Floor, 0.5 is all okay. Only 1-complement cut off ( towards 0 ) is not allowed. So be careful with floats!
 	transform_vertex_forwards(ver:number[]):Vec3_frac{
 		this.mul
-		let screen=new Array<frac
+		let screen=new Array<Frac
 		y=div // for for loop across scanline
 		// for Bresenham
 		nominator[]
@@ -152,7 +202,7 @@ class Projector extends Position{ // camera
 
 	// todo: some weird OOP pattern to shift direction of transformation. Mix with a custom number type which can be fixed, float, variablePrecision
 
-	y_at_intersection(e:vec3[]):frac{
+	y_at_intersection(e:vec3[]):Frac{
 		if (e[0].id==border_top) {y=border_top;return} // portal renderer  or  even BSP with coverage-buffer wants this
 
 		let beam=e[0].crossProduct(e[1]) // edges are planes with a normal in a beam tree
@@ -192,17 +242,18 @@ class Projector extends Position{ // camera
 class Point_Tex2{
 	point:number[]
 	tex:number[]
+
 }
 
 class Texture{ // similar to (clipped) edge
 	p:Polygon // reference .. bidirectional .. I may need to pull some data like base:
 	base:number[] // texture (0,0) in space .. So not so great for environment mapping, but really great later maybe for triangles which are extended into perfectly flat, not necessarily concave polygons
 	// Material texture (tiles) should not be squeezed .. but UV unwrapped organic texture (globe) needs to
-	spanning:vec3[]
+	spanning:Vec3[]
 	innerLoop:number[][] // scree{x,y} -> u v denominator  // the dependency graph may depend on clipping. The form of the function is independent from it.
-	constructor(vertices:Point_Tex2[]){ 
-		if (clippling){ // point array count=2  // important for level
-			this.base=vertices[0].point
+	constructor(vertex:Point_Tex2[]){ 
+		if (clippling){ // point array count=2  // important for level . Perspective correction is mandatory to differentiate the Jag from 3do and PSX
+			this.base=vertex[1].point
 
 			// inverse within a plane at least gives us a more simple determinant in the denominator .. hm lenght(cross product) . So somehow we now have a square-root here?
 			// nominator xyz * innvers = uv  . not square.
@@ -210,21 +261,58 @@ class Texture{ // similar to (clipped) edge
 			/*
 				Here is the geometric version
 			*/
-			let camera:vec3
+			for(let i=0;i<2;i++)
+				this.spanning[i]=new Vec3(vertex.slice(0+i,2+i).map(p=>p.point))
+			let camera:Vec3
 			let normal=this.spanning[0].crossProduct(this.spanning[1])   // length should not matter .. inverse does not care. 
-			let hasToBeCoverdBy1=this.spanning[0].crossProduct(normal).innerProduct(camera) // just the inversion equation (transpose is a bit hidden? With inner product here the other direction needs left multiply)
-			let denominator=this.spanning[0].crossProduct(normal).innerProduct(this.spanning[0]) // obviously gives 1 when needed
+			this.spanning[2]=normal // feels as silly as the 1/z in the forward path . This screams for float .. or at least reinforces the fact that edges in a level should be about 1m long. 1mm cammera precision, 1km level ( or viewing distance??)
+			//let hasToBeCoverdBy1=this.spanning[0].crossProduct(normal).innerProduct(camera) // just the inversion equation (transpose is a bit hidden? With inner product here the other direction needs left multiply)
+			//let denominator=this.spanning[0].crossProduct(normal).innerProduct(this.spanning[0]) // obviously gives 1 when needed
+			
 			// I need height anyway (whatever "unit"). So 3x3 inverse. Then transform both (same unit) camera position and viewing direction into this. Then triangle divide as in checker board.
 			// The normal is hence justified. Maybe I could compile the level and find small normals.
 			// multiply with spanning UV
+
+			// inverter , check for normel.cross => early out 0
+			// spanning is vectorAdd . We don't need to transpose. Inverse is already innerP.
+			let inverse=new Matrix_frac
+			for (let i=0;i<2;i++){
+				let v=this.spanning[(i+1)%3].crossProduct(this.spanning[(i+2)%3])
+	
+				inverse.nominator[i]=v //.inverse(spanning2d)
+			}		
+			inverse.denominator=inverse.nominator[0].innerProduct(this.spanning[0])	// count down in the loop above to have the values in registers
 		}else{ // imporant for high detail enenmies and affine texture mapping on small triangles
 			/*
 			2d 
 				spanning Points
-				invert  .
+				invert  . Only place with 2d invert
 			*/
-			if (affine){
-				Mul spanning UV
+			if (affine){ // check if (delta z^2)/2 * (x+y ) < threshold ( value)  or cost : block division?
+				let spanning2d=new Array<Vec>(2)
+				for(let i=0;i<2;i++){
+					spanning2d[i]=new Vec([vertex[i+1].point,vertex[0].point]) // temporary
+				}
+				let inverse=new Matrix_frac
+				for (let i=0;i<2;i++){
+					let v=new Vec([[2]])
+					v[0+i]=-spanning2d[1-i].v[1-i]
+					v[1-i]=+spanning2d[0+i].v[1-i]
+					inverse.nominator[i]=v //.inverse(spanning2d)
+				}
+				inverse.denominator=0
+				for (let i=0;i<2;i++){				
+					inverse.denominator=spanning2d[0].v[0+i]*spanning2d[1].v[1-i]-inverse.denominator
+				}				
+				// todo: tests from CPU sim
+				// UV
+				for(let i=0;i<2;i++){
+					spanning2d[i]=new Vec([vertex[i+1].point,vertex[0].tex]) // temporary
+				}		
+				let deltas=inverse.mul(spanning2d)
+
+				//
+
 			}else{
 				// do I really want this?
 				U=U/Z
